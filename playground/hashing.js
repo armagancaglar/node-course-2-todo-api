0c3fcb3d1770baca2baab5fd1fdd27060688e843
Armagan Caglar <c.armagancaglar@gmail.com>
2018-03-29 20:04:34 +0300

    Hash user password before seving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc';
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+
+var hashedPassword = '$2a$10$zJxqGinaN8RwDOomIrLmP.zRzyWQgnEQdBISHb/VEJWJCQb4r3era';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
 //jwt.verify
 
 // var message = 'I am user number 3';
